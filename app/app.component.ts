0a98e4c0eda5bfdf08129dda63a5c2d4dd723e40
John <jldjld503@gmail.com>
2016-06-02 18:49:16 -0700

    added class for component app class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
@@ -6,9 +7,22 @@ import { Component } from 'angular2/core';
     <div class="container">
         <h1>MEALR</h1>
         <h4>List of Meals Eaten Today</h4>
+        <meal-list
+        [mealList]="meals"
+        (onMealSelect)="mealWasSelected($event)">
+      </meal-list>
     </div>
   `
 })
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Breakfast", "Ate some carrots", "100", 0),
+      new Meal("Lunch", "Ate a Rabbit", "150", 1)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log('parent', clickedMeal);
+  }
 }
