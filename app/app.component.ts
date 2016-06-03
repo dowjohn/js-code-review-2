ccd00a52ce5ac00cd8f127dce9951e816d724b6e
John <jldjld503@gmail.com>
2016-06-02 18:40:00 -0700

    created skeleton and model for meal constructor

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+        <h1>MEALR</h1>
+        <h4>List of Meals Eaten Today</h4>
+    </div>
+  `
+})
+export class AppComponent {
+
+}
